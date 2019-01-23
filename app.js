0b38a3b8b3e39f1732026951a6f8f06cf25e2297
Mav <mavericktesini@gmail.com>
2019-01-23 13:21:04 -0500

    res 302 and create server file for message field

--- app.js
+++ app.js
@@ -1,12 +1,20 @@
 const http = require('http');
+const fs = require('fs');
 
 const server = http.createServer((req, res) => {
   const url = req.url;
+  const method = req.method;
   if (url === '/') {
     res.write('<html>');
     res.write('<head><title>Root Message</title></head>');
     res.write('<body><h1>Submit Server Data</h1><form action="/message" method="POST"><input type="text" name="message" /><button type="submit">Send</button></form></body>')
     res.write('</html>');
+    return res.end();
+  }
+  if (url === '/message' && method === 'POST') {
+    fs.writeFileSync('message.txt', 'Sample Text');
+    res.statusCode = 302;
+    res.setHeader('Location', '/');
     return res.end();
   }
   res.setHeader('Content-Type', 'text/html');
