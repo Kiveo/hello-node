68a3b048f1bcb8f0f8f6c89160e76428abc6075c
Mav <mavericktesini@gmail.com>
2019-01-23 11:56:07 -0500

    create res writes for demo

--- app.js
+++ app.js
@@ -3,6 +3,10 @@ const http = require('http');
 const server = http.createServer((req, res) => {
   console.log(req.url, req.method, req.headers);
   // process.exit()
+  res.setHeader('Content-Type', 'text/html');
+  res.write('<html><head><title>Sample Server</title></html>');
+  res.end();
 });
+//can be seen via dev tools. Use network for server info and peak at title of page
 
 server.listen(3000)
