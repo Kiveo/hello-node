a83e65d6891de17011409e0088ffba8cd122f3cc
Mav <mavericktesini@gmail.com>
2019-01-23 12:02:38 -0500

    create notes

--- app.js
+++ app.js
@@ -1,11 +1,15 @@
 const http = require('http');
 
 const server = http.createServer((req, res) => {
-  console.log(req.url, req.method, req.headers);
-  // process.exit()
-  res.setHeader('Content-Type', 'text/html');
-  res.write('<html><head><title>Sample Server</title></html>');
-  res.end();
+  const url = req.url;
+  if (url === '/') {
+    res.setHeader('Content-Type', 'text/html');
+    res.write('<html>');
+    res.write('<head><title>Node App</title>');
+    res.write('</html>');
+    res.end();
+  }
+  
 });
 //can be seen via dev tools. Use network for server info and peak at title of page
 
---
+++ notes.md
@@ -0,0 +1,4 @@
+To-Do
+  create server with different route responses
+
+  Load a page where user enters data and server saves into a file
